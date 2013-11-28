a4af3634f00d75cd398ece9b3ab61d195b60343d
Alessandro Loffredo <cyberalexgm@gmail.com>
2013-11-28 16:03:30 +0100

    Update EventInterface.php

--- src/FreeAgent/Bitter/Event/EventInterface.php
+++ src/FreeAgent/Bitter/Event/EventInterface.php
@@ -10,6 +10,6 @@ interface EventInterface
     public function __construct($eventName, \DateTime $dateTime);
     public function getEventName();
     public function getDateTime();
-    public function getDateTimeFormated();
+    //public function getDateTimeFormated();
     public function getKey();
 }
